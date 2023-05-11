345a6546a707fddf1f72db69a433e0f61b050035
37erenyldrmd <129751275+37erenyldrmd@users.noreply.github.com>
2023-05-11 22:18:44 +0300

    Update _document.jsx

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -13,12 +13,12 @@ class MyDocument extends Document {
             name="description"
             content="How about listening to music without pausing? Create your own playlists with 750+ supported sound platforms and start listening."
           />
-          <meta name="twitter:card" content="summary" />
-          <meta name="twitter:site" content="@MusicMaker" />
-          <meta name="twitter:creator" content="@MusicMaker" />
-          <meta property="og:url" content="https://musicmaker.vercel.app" />
+          <meta name="instagram:card" content="37erenyldrm" />
+          <meta name="instagram:site" content="@37erenyldrm" />
+          <meta name="instagram:creator" content="@37erenyldrm" />
+          <meta property="og:url" content="https://pronto-bot.vercel.app" />
           <meta property="og:type" content="website" />
-          <meta property="og:title" content="MusicMaker" />
+          <meta property="og:title" content="ProntoMaker" />
           <link
             rel="icon"
             href="/img/logo2.png"
@@ -26,14 +26,14 @@ class MyDocument extends Document {
           />
           <meta
             property="og:description"
-            content="How about listening to music without pausing? Create your own playlists with 750+ supported sound platforms and start listening."
+            content="Hiç ara vermeden müzik dinlemeye ne dersiniz? 750'den fazla desteklenen ses platformuyla kendi çalma listelerinizi oluşturun ve dinlemeye başlayın."
           />
           <meta property="og:image" content="/img/logo.jpg" />
-          <meta property="og:image:alt" content="MusicMaker" />
+          <meta property="og:image:alt" content="ProntoMaker" />
           <meta property="og:locale" content="en_GB" />
           <meta
             property="og:site_name"
-            content="MusicMaker"
+            content="ProntoMaker"
           />
           <meta name="theme-color" content="#6108dd" />
           <link rel="icon" href="/img/logo.jpg" type="image/x-icon" />

755b1722a71e90fb86b94ec4f057b4683476e687
37erenyldrmd <129751275+37erenyldrmd@users.noreply.github.com>
2023-05-11 22:09:42 +0300

    Update 500.jsx

--- pages/500.jsx
+++ pages/500.jsx
@@ -1,6 +1,6 @@
 import ErrorPage from '../components/ErrorPage';
 const UnkownPage = () => {
-    return <ErrorPage code={500} message={"Ah. 500 Dahili Sunucu Hatası var gibi görünüyor. Lütfen daha sonra tekrar deneyiniz!"} />
+    return <ErrorPage code={500} message={"Kahretsin! Sunucu Hatası var gibi görünüyor. Lütfen daha sonra tekrar deneyiniz!"} />
 }
 
 export default UnkownPage;

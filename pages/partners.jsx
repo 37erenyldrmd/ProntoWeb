e9cfcfeb142436a76c78495500c4f18b982325be
37erenyldrmd <129751275+37erenyldrmd@users.noreply.github.com>
2023-05-15 22:40:53 +0300

    Update partners.jsx

--- pages/partners.jsx
+++ pages/partners.jsx
@@ -9,21 +9,21 @@ export default function Partners() {
       </div>
                 <h1 className="py-7 animateHeader text-4xl font-extrabold text-center text-white">
                     <i className="fal fa-stars text-amber-400 mr-2" />
-                    Partners
+                    Partnerlerimiz
                 </h1>
             </div>
 
             <div className="lg:max-w-screen-lg mt-10 mx-auto grid grid-cols-1 md:grid-cols-2 gap-4">
 
             <div className="flex flex-col justify-center text-white rounded">
-            <a href="https://discord.gg/6XGqdgE">
+            <a href="https://discord.gg/veR37TpbF3">
                         <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
                             <img className="rounded-full h-32 w-32" src="https://cdn.discordapp.com/icons/676124992919830548/68ef946cf7cc39e4063eed4d446f6dda.webp" />
                             <div>
-                                <h1 className="leading-none text-3xl font-bold text-white">Code Share</h1>        
+                                <h1 className="leading-none text-3xl font-bold text-white">Pronto Destek</h1>        
                                 <div className="flex items-center mt-1">
                                     <p className="font-normal font-sm">
-                                        Share Your Codes, Can You Keep Your Projects Active Forever. World's Largest Website with Discord Interface!
+                                        Pronto Ekibiyle Botumuz Kısa Süre İçerisinde Daha İyi Özelliklere Sahip Olabilecek
                                     </p>
                                 </div>     
                             </div>
@@ -36,10 +36,10 @@ export default function Partners() {
                             <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
                             <img className="rounded-full h-32 w-32" src="https://cdn.discordapp.com/icons/924312493537509407/721de16575a9048c9c6980613ebb99f0.webp" />
                             <div>
-                                <h1 className="leading-none text-3xl font-bold text-white">The Developers</h1>        
+                                <h1 className="leading-none text-3xl font-bold text-white">VoidTopia | play.VoidTopia.pw</h1>        
                                 <div className="flex items-center mt-1">
                                     <p className="font-normal font-sm">
-                                    You can get help on software and similar issues or you can spend time chatting. How about being a cool developer?
+                                    VoidTopia Minecraft Sunucusu Geliştirilmesi Devam Eden En İyi Minecraft Sunucusu Pronto Desteginde Katkısı Büyüktür.
                                     </p>
                                 </div>     
                             </div>
